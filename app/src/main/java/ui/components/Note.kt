a8d2b9c5b09afffe445235a5957df9bca225386d
bat9k <ilia34775@gmail.com>
2024-03-15 14:47:38 +0300

    update Note

--- app/src/main/java/ui/components/Note.kt
+++ app/src/main/java/ui/components/Note.kt
@@ -2,8 +2,26 @@ package ui.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.topic2.android.notes.theme.rwGreen
+import androidx.compose.foundation.background
 
 @Composable fun Note(){
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .background(rwGreen)
+    )
+    Text(text = "Заголовок", maxLines = 1)
+    Text(text = "Содержимое", maxLines = 1)
+    Checkbox(
+        checked = false, onCheckedChange = { },
+        modifier = Modifier.padding(start = 8.dp)
+    )
 
 }
 @Preview
